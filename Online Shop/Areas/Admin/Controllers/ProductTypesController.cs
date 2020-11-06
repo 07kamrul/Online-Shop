e45e6e7e4ff70441d9bd434b1382593962d5b385
07kamrul <mdkamrulhasanewu@gmail.com>
2020-11-07 02:44:21 +0600

    Create (Lecture-10)

---
+++ Online Shop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Online_Shop.Data;
+using Online_Shop.Models;
+
+namespace Online_Shop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var data = _db.ProductTypes.ToList();
+            return View(data);
+        }
+        //Create Get Action Method
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+            }
+            return View(productTypes);
+        }
+    }
+}
